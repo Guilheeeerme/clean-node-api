a7a901bcf4c02f0e661b80c72ece322ffcc06f16
Guilheeeerme <guilherme_moura.10222@outlook.com>
2021-07-19 21:38:56 -0300

    ensure LoginRouter return 500 if EmailValidator throws

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -28,6 +28,15 @@ const makeEmailValidator = () => {
   return emailValidatorSpy;
 };
 
+const makeEmailValidatorWIthError = () => {
+  class EmailValidatorSpy {
+    isValid() {
+      throw new Error();
+    }
+  }
+  return new EmailValidatorSpy();
+};
+
 const makeAuthUseCase = () => {
   class AuthUseCaseSpy {
     async auth(email, password) {
@@ -222,5 +231,20 @@ describe("Login Router", () => {
     const httpResponse = await sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
+  });
+
+  test("Should return 500 if EmailValidator throws", async () => {
+    const authUseCaseSpy = makeAuthUseCase();
+    const emailValidatorSpy = makeEmailValidatorWIthError();
+    const sut = new LoginRouter(authUseCaseSpy, emailValidatorSpy);
+    const httpRequest = {
+      body: {
+        email: "any_email@mail.com",
+        password: "any_password",
+      },
+    };
+
+    const httpResponse = await sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
   });
 });
