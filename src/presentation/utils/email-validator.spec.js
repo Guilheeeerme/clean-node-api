4f7c32c01251398499e82b52fd16e87e2fd6ccfb
Guilheeeerme <guilherme_moura.10222@outlook.com>
2021-07-20 23:30:11 -0300

    ensure EmailValidator calls validator with correct email

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -26,13 +26,11 @@ describe("Email Validator", () => {
 
     expect(isEmailInvalid).toBe(false);
   });
-});
 
-// Testando o email com validator como exemplo aprendi que: Precisamos mockar porque queremos garantir
-// que vai funcionar o que nós passarmos de fato, caso não fizesse isso, é como se simplesmente
-// nós estivesse testando apenas o validator, e isso nao faz tanto sentido, porque se estamos no ponto
-// que decidimos usar essa lib é porque no minimo ela funciona corretamente, certo?
-// Então testamos independente dela... porque até se mudar de lib, como mockamos, não quebra os testes
-// Obs: Se nós tivermos uma pasta chamada __mocks__ e o arquivo em js for o mesmo nome da lib
-// que queremos testar, mesmo se importamos apenas ('nome da lib') e nao o caminho do mock, o jest ainda sim
-// entende e vai chamar o mock, e nao a lib de fato.
+  test("should call validator with correct email", () => {
+    const sut = makeSut();
+    sut.isValid("any_email@mail.com");
+
+    expect(validator.email).toBe("any_email@mail.com");
+  });
+});
