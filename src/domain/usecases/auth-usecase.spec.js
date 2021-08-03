dc3f0fb248652ca6729199c7108b4f5885f6985f
Guilheeeerme <guilherme_moura.10222@outlook.com>
2021-08-02 22:18:31 -0300

    ensure AuthUseCase calls TokenGenerator with correct userId

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -17,6 +17,20 @@ const makeEncrypter = () => {
   return encrypterSpy;
 };
 
+const makeTokenGenerator = () => {
+  class TokenGeneratorSpy {
+    async generate(userId) {
+      this.userId = userId;
+
+      return this.accessToken;
+    }
+  }
+  const tokenGeneratorSpy = new TokenGeneratorSpy();
+  tokenGeneratorSpy.accessToken = "any_token";
+
+  return tokenGeneratorSpy;
+};
+
 const makeLoadUserByEmailRepository = () => {
   class LoadUserByEmailRepositorySpy {
     async load(email) {
@@ -27,6 +41,7 @@ const makeLoadUserByEmailRepository = () => {
 
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
   loadUserByEmailRepositorySpy.user = {
+    id: "any_id",
     password: "hashed_password",
   };
 
@@ -36,10 +51,15 @@ const makeLoadUserByEmailRepository = () => {
 const makeSut = () => {
   const encrypterSpy = makeEncrypter();
   const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository();
+  const tokenGeneratorSpy = makeTokenGenerator();
 
-  const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy);
+  const sut = new AuthUseCase(
+    loadUserByEmailRepositorySpy,
+    encrypterSpy,
+    tokenGeneratorSpy
+  );
 
-  return { sut, loadUserByEmailRepositorySpy, encrypterSpy };
+  return { sut, loadUserByEmailRepositorySpy, encrypterSpy, tokenGeneratorSpy };
 };
 
 describe("Auth UseCase", () => {
@@ -114,5 +134,13 @@ describe("Auth UseCase", () => {
     expect(encrypterSpy.hashedPassword).toBe(
       loadUserByEmailRepositorySpy.user.password
     );
+  });
+
+  test("Should call TokenGenerator with correct userId", async () => {
+    const { sut, loadUserByEmailRepositorySpy, tokenGeneratorSpy } = makeSut();
+
+    await sut.auth("valid_email@mail.com", "valid_password");
+
+    expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user.id);
   });
 });
